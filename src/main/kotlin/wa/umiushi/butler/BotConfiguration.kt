918412749d0c8cf9f12313ac63ceabecca0cf561
umi-ushi <umiushi.me+dev@gmail.com>
2025-05-07 17:25:31 +0900

    [add] mention event handler

--- src/main/kotlin/wa/umiushi/butler/BotConfiguration.kt
+++ src/main/kotlin/wa/umiushi/butler/BotConfiguration.kt
@@ -1,6 +1,7 @@
 package wa.umiushi.butler
 
 import com.slack.api.bolt.App
+import com.slack.api.model.event.AppMentionEvent
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -10,6 +11,20 @@ class BotConfiguration {
     @Bean
     fun slackApp(): App {
         return App().apply {
+            event(AppMentionEvent::class.java, { req, ctx ->
+
+                val args = req.event.text.split(Regex("\\s+"))
+
+                val message = ctx.client().chatPostMessage {
+                    it.channel(req.event.channel)
+                        .threadTs(req.event.ts)
+                        .text("<@${req.event.user}> 要求は ${args.joinToString(",")} ですか？ :two_hearts:")
+                }
+                if (!message.isOk) {
+                    ctx.logger.error("chat.postMessage failed: ${message.error}")
+                }
+                ctx.ack()
+            })
             command("/hello", { _, ctx -> ctx.ack("What's up?") })
         }
     }
